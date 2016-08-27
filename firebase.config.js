08371172b3ad561f2eae165c7bfd9e1db4ef9b94
Pete Brousalis <brousapg@gmail.com>
2016-08-27 15:00:58 -0500

    init from create-react-app

---
+++ firebase.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    apiKey: "AIzaSyDuKr8FL1kX_DYIjzTbYySSkjClt133rqk",
+    authDomain: "react-705f7.firebaseapp.com",
+    databaseURL: "https://react-705f7.firebaseio.com",
+    storageBucket: "react-705f7.appspot.com",
+}
